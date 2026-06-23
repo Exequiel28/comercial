f06bc50e514eddede1044aba27afeb48e832056f
unknown <exequielantonio28@gmail.com>
2026-06-23 16:00:51 -0600

    Cambios logo en la barra

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,9 +5,9 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Comercial Portillo') : config('app.name', 'Comercial Portillo') }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="/logop.png" sizes="any">
+<link rel="icon" href="/logop.png" type="image/svg+xml">
+<link rel="apple-touch-icon" href="/logop.png">
 
 @fonts
 
